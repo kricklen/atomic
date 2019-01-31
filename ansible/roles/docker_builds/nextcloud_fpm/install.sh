712590d66bc0c915f95ced63cf88a6004a27b37f
root <root@localhost.localdomain>
2019-01-31 05:58:04 +0100

    Add auto install for calendar and contacts to nextcloud_fpm, add default admin

--- ansible/roles/docker_builds/nextcloud_fpm/install.sh
+++ ansible/roles/docker_builds/nextcloud_fpm/install.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-/usr/src/nextcloud/occ maintenance:install --data-dir /var/www/html --admin-user admin --admin-pass admin
-
 export occ_cmd='/var/www/html/occ'
+
+# Create link for nextcloud log file to write messages to stdout
+echo "Linking nc_docker.log to stdout"
+ln -s /dev/stdout /var/www/nc_docker.log
+chown www-data:www-data /var/www/nc_docker.log
+
+$occ_cmd maintenance:install --admin-user admin --admin-pass admin
+echo "Setting log file to c_docker.log"
 $occ_cmd log:file --rotate-size 0 --file /var/www/nc_docker.log
+echo "Installing calendar app"
+$occ_cmd app:install calendar
+echo "Installing contacts app"
+$occ_cmd app:install contacts
+echo "Done"
+
+exec "$@"
