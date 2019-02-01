27f3b5010247fc56c3eec490a5f9905e897f8c8f
markus_mayerle <mayerle.m@wk-edv.de>
2019-02-01 10:04:21 +0100

    Fix for nextcloud install.sh

--- ansible/roles/docker_builds/nextcloud_fpm/install.sh
+++ ansible/roles/docker_builds/nextcloud_fpm/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+/usr/src/nextcloud/occ maintenance:install --data-dir /var/www/html --admin-user admin --admin-pass admin
+
 export occ_cmd='/var/www/html/occ'
-$occ_cmd maintenance:install --admin-user admin --admin-pass admin
 $occ_cmd log:file --rotate-size 0 --file /var/www/nc_docker.log
