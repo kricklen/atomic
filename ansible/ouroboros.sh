afebe848bd598138f544770974251a926699b1c1
root <root@tuxhost.fritz.box>
2019-03-28 18:25:12 +0100

    Add build.sh for samba docker, add ouroboros.sh

--- ansible/ouroboros.sh
+++ ansible/ouroboros.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker run -h ouroboros --name ouroboros -e CLEANUP=true -e INTERVAL=300 -e LOG_LEVEL=info -e SELF_UPDATE=true -e IGNORE="mongo influxdb postgres mariadb" -e TZ="Europe/Berlin" -v /var/run/docker.sock:/var/run/docker.sock -d pyouroboros/ouroboros
+#docker run --privileged --rm -h ouroboros --name ouroboros -e CLEANUP=true -e INTERVAL=300 -e LOG_LEVEL=info -e SELF_UPDATE=true -e IGNORE="mongo influxdb postgres mariadb" -e TZ="Europe/Berlin" -v /var/run/docker.sock:/var/run/docker.sock pyouroboros/ouroboros
 
+docker run --privileged -h ouroboros --name ouroboros -e CLEANUP=true -e INTERVAL=300 -e LOG_LEVEL=info -e SELF_UPDATE=true -e IGNORE="mongo influxdb postgres mariadb" -e TZ="Europe/Berlin" -v /var/run/docker.sock:/var/run/docker.sock -d pyouroboros/ouroboros
