4e3b309882d16f754031dcfaa49b0b31a0d7fc44
markus_mayerle <markus.mayerle@wk-it.com>
2019-07-30 09:44:25 +0200

    Add missing env.sh

---
+++ ansible/roles/stack/templates/env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Global vars
+export timezone=Europe/Berlin
+
+# DDClient
+export ddclient_user_id=$(id -u ddclient)
+export ddclient_group_id=$(id -u ddclient)
+export ddclient_config_dir=/mnt/ddclient
