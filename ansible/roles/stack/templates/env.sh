f17b0e91deb640d055c3438504d110b9769ee5a7
markus_mayerle <markus.mayerle@wk-it.com>
2019-07-31 11:06:14 +0200

    Test with docker_swarm_service

--- ansible/roles/stack/templates/env.sh
+++ ansible/roles/stack/templates/env.sh
@@ -8,4 +8,11 @@ export ddclient_user_id=$(id -u ddclient)
 export ddclient_group_id=$(id -u ddclient)
 export ddclient_config_dir=/mnt/ddclient
 
+# Nextcloud
+export nextcloud_user_id=$(id -u nextcloud)
+export nextcloud_group_id=$(id -g nextcloud)
+export nextcloud_db_dir=/mnt/nextcloud/db
+export nextcloud_www_dir=/mnt/nextcloud/www
+export nextcloud_nginx_dir=/mnt/nextcloud/nginx
+
 docker stack deploy -c ./docker-compose.yml HomeServer
