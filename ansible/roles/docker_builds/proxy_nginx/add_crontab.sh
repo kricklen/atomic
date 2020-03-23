359767273839a78a25b49f0ae65a26cc2112d836
root <root@tuxhost.fritz.box>
2020-03-23 10:47:24 +0100

    Simplified nginx reload, still needs cron in docker

--- ansible/roles/docker_builds/proxy_nginx/add_crontab.sh
+++ ansible/roles/docker_builds/proxy_nginx/add_crontab.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/bin/crontab -l | { cat; echo "20 5 * * 0 /nginx_reload.sh"; } | crontab -
+#/usr/bin/crontab -l | { cat; echo "20	5	*	*	0	/nginx_reload.sh"; } | crontab -
+/usr/bin/crontab -l | { cat; echo "20	5	*	*	0	/usr/sbin/nginx -s reload"; } | crontab -
