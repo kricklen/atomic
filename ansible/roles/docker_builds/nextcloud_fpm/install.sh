cd5b27ea20400a31f2c7428e1b42ff1bae02568b
root <root@tuxhost.fritz.box>
2020-04-07 13:30:44 +0200

    Add cron job to nextcloud-fpm image

--- ansible/roles/docker_builds/nextcloud_fpm/install.sh
+++ ansible/roles/docker_builds/nextcloud_fpm/install.sh
@@ -7,6 +7,13 @@ echo "Linking nc_docker.log to stdout"
 ln -s /dev/stdout /var/www/nc_docker.log
 chown www-data:www-data /var/www/nc_docker.log
 
+# Create cron job to call cron.php every 5 mins
+echo "Creating cron job for nextcloud"
+/usr/bin/crontab -u www-data -l | { cat; echo "20   5       *       *       0       /usr/local/bin/php /var/www/html/cron.php"; } | crontab -u www-data -
+# Start crond
+/usr/sbin/crond
+
+# Configure nextcloud
 $occ_cmd maintenance:install --admin-user admin --admin-pass admin
 echo "Setting log file to c_docker.log"
 $occ_cmd log:file --rotate-size 0 --file /var/www/nc_docker.log
