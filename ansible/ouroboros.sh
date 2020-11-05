e68577b0bfcabf82ca8f5cd32e862a7b8c835c2c
root <root@tuxhost.fritz.box>
2020-11-05 11:02:56 +0100

    Set ouroboros interval to 1h

--- ansible/ouroboros.sh
+++ ansible/ouroboros.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #docker run --privileged --rm -h ouroboros --name ouroboros -e CLEANUP=true -e INTERVAL=300 -e LOG_LEVEL=info -e SELF_UPDATE=true -e IGNORE="mongo influxdb postgres mariadb" -e TZ="Europe/Berlin" -v /var/run/docker.sock:/var/run/docker.sock pyouroboros/ouroboros
 
-docker run --privileged -h ouroboros --restart unless-stopped --name ouroboros -e CLEANUP=true -e INTERVAL=300 -e LOG_LEVEL=info -e SELF_UPDATE=true -e IGNORE="mongo influxdb postgres mariadb" -e TZ="Europe/Berlin" -v /var/run/docker.sock:/var/run/docker.sock -d pyouroboros/ouroboros
+docker run --privileged -h ouroboros --restart unless-stopped --name ouroboros -e CLEANUP=true -e INTERVAL=3600 -e LOG_LEVEL=info -e SELF_UPDATE=true -e IGNORE="mongo influxdb postgres" -e TZ="Europe/Berlin" -v /var/run/docker.sock:/var/run/docker.sock -d pyouroboros/ouroboros
