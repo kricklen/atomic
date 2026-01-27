16ebbe0e45fe9ac37b71c7c741711dae791287cc
kricklen <dirion@gmx.net>
2026-01-27 09:47:39 +0100

    add systemd service and timer for nextcloud cron, update readme

--- ansible/roles/docker_builds/nextcloud_fpm/install.sh
+++ ansible/roles/docker_builds/nextcloud_fpm/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 export occ_cmd='/var/www/html/occ'
 
@@ -6,12 +7,6 @@ export occ_cmd='/var/www/html/occ'
 echo "Linking nc_docker.log to stdout"
 ln -s /dev/stdout /var/www/nc_docker.log
 chown www-data:www-data /var/www/nc_docker.log
-
-# Create cron job to call cron.php every 5 mins
-echo "Creating cron job for nextcloud"
-/usr/bin/crontab -u www-data -l | { cat; echo "20   5       *       *       0       /usr/local/bin/php /var/www/html/cron.php"; } | crontab -u www-data -
-# Start crond
-/usr/sbin/crond
 
 # Configure nextcloud
 $occ_cmd maintenance:install --admin-user admin --admin-pass admin
