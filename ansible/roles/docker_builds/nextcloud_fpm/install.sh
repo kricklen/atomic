fdcff33c3aa3bb3da94906455dd31120bb26cacb
kricklen <dirion@gmx.net>
2026-01-27 22:14:16 +0100

    cleanup nextcloud-fpm install.sh

--- ansible/roles/docker_builds/nextcloud_fpm/install.sh
+++ ansible/roles/docker_builds/nextcloud_fpm/install.sh
@@ -6,10 +6,5 @@ export occ_cmd='/var/www/html/occ'
 # Configure nextcloud
 echo "Setting log file to php://stdout"
 $occ_cmd log:file --rotate-size 0 --file php://stdout
-echo "Installing calendar app"
-$occ_cmd app:install calendar
-echo "Installing contacts app"
-$occ_cmd app:install contacts
-echo "Done"
 
 exec "$@"
