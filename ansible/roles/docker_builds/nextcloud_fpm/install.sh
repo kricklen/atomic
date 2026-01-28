6e1f09c24c6740778cdd62202b9e336d88f5a7d0
kricklen <dirion@gmx.net>
2026-01-28 13:25:47 +0100

    remove logfile config from nextcloud_fpm install.sh

--- ansible/roles/docker_builds/nextcloud_fpm/install.sh
+++ ansible/roles/docker_builds/nextcloud_fpm/install.sh
@@ -4,7 +4,7 @@ set -e
 export occ_cmd='/var/www/html/occ'
 
 # Configure nextcloud
-echo "Setting log file to php://stdout"
-$occ_cmd log:file --rotate-size 0 --file php://stdout
+#echo "Setting log file to php://stdout"
+#$occ_cmd log:file --rotate-size 0 --file php://stdout
 
 exec "$@"
